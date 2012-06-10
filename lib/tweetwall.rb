12479c3a5bd90a163d2eb373473710384cd67926
Matthew Grigajtis <matthew@grigajtis.org>
2012-06-09 23:06:21 -0400

    fixed a couple of bugs

--- lib/tweetwall.rb
+++ lib/tweetwall.rb
@@ -1,16 +1,21 @@
 require "tweetwall/version"
+require "twitter"
 
 module Tweetwall
-  def print_tweets(twitter_user, number_of_tweets=5)
+  def self.print_tweets(twitter_user, number_of_tweets=5)
     tweet_wall_html = ""
     twitter_feed = Twitter.user_timeline(twitter_user)[0..number_of_tweets]
+    twitter_image = Twitter.user(twitter_user).profile_image_url
 
     tweet_wall_html = tweet_wall_html + '<div id="tweet_wall">'
     twitter_feed.each do |tweet|
+      tweet_wall_html = tweet_wall_html + '<div class="tweet_image">'
+      tweet_wall_html = tweet_wall_html + '<img src="' + twitter_image + '" alt="" class="tweet_image" /> '
       tweet_wall_html = tweet_wall_html + '<span class="tweet_text">' + tweet.text + '</span><br />'
+      tweet_wall_html = tweet_wall_html + '</div>'
     end
     tweet_wall_html = tweet_wall_html + '</div>'
 
-    tweet_wall
+    tweet_wall_html
   end
 end
